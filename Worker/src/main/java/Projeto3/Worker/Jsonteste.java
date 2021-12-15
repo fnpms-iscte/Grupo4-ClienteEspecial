c87de7d5a6f8053f12ec3e62f898e665cdf0dc4e
Hunuru9 <hacls@iscte-iul.pt>
2021-12-15 01:58:12 +0000

    update jsonteste

--- Worker/src/main/java/Projeto3/Worker/Jsonteste.java
+++ Worker/src/main/java/Projeto3/Worker/Jsonteste.java
@@ -12,12 +12,12 @@ public class Jsonteste {
 	public JsonObject jsonObject;
 
 	public Jsonteste(String id) {
-		this.a = "{ 'horarios' : [{ 'horario1' : [{\"Idade\":\"21222\","
+		this.a = "{ 'horarios' : [{ 'nome': 'Horario 1','horario' : [{\"Idade\":\"21222\","
 				+ "\"Local\":\"Almada\",\"Nome\":\"Francisco\"},"
 				+ "{\"Idade\":\"2222\",\"Local\":\"Sintra\",\"Nome\":\"Hugo\"},{\"Idade\":\"223232\","
 				+ "\"Local\":\"Sintra\",\"Nome\":\"Chico\"}] , "
-				+ "'metricas' : { lista : [{'furos' : 0 , 'aulas_sem_salas' : 15}] , "
-				+ "'detalhe' : 'Poucos furos'} }, {'horario2' : [\r\n"
+				+ "'metricas' : { lista : [{'nome' : 'Furos', 'valor': 90} ,{'nome' : 'Aulas sem salas', 'valor': 37}] , "
+				+ "'detalhe' : 'poucos'} }, {'nome': 'Horario 2', 'horario' : [\r\n"
 				+ "  {\r\n"
 				+ "    \"Idade\": 55555,\r\n"
 				+ "    \"Local\": \"Almada\",\r\n"
@@ -33,7 +33,7 @@ public class Jsonteste {
 				+ "    \"Local\": \"Sintra\",\r\n"
 				+ "    \"Nome\": \"Chico\"\r\n"
 				+ "  }\r\n"
-				+ "], 'metricas' = {lista :[{'furos' : 1110 , 'aulas_sem_salas' : 15}], detalhe : 'Muitos furos' }}] ," + " " + "'id_client': " + id + "}";
+				+ "], 'metricas' = {lista :[{'nome' : 'Furos', 'valor': 90} ,{'nome' : 'Aulas sem salas', 'valor': 37}], detalhe : 'muitos' }}] ," + " " + "'id_client': " + id + "}";
 
 		this.jsonObject = (JsonObject) JsonParser.parseString(a);
 
